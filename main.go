3019d0fcafe8135169cf8597d122ad5aa0b15fa0
Raymond Karumba <rkarumba@outlook.com>
2024-04-10 12:33:33 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+// main is the entry point of the program.
 func main() {
-	fmt.Println("Hello World!")
+	// Create a new HTTP server.
+	server := &http.Server{
+		Addr:    ":3000",                        // Listen on port 3000.
+		Handler: http.HandlerFunc(basicHandler), // Set the handler function.
+	}
+
+	// Start the server and listen for incoming requests.
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to Listen to Server:", err)
+	}
 }
 
-
+// basicHandler is the handler function for the root route.
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World")) // Write "Hello World" as the response.
+}
